1772e9ea69a0df3ba535fe9a741deba68894b69f
smndrtmn <simon.dertmann@gmail.com>
2023-07-27 17:33:25 +0200

    v2

--- script.js
+++ script.js
@@ -10,9 +10,26 @@ const transactionsTBody = document.querySelector('#transactions tbody');
 
 // Simulierte Datenbank
 const database = [
-  { firstName: 'Max', lastName: 'Mustermann', birthDate: '2011-01-01', balance: 10.50, transactions: [] },
-  { firstName: 'Max', lastName: 'Meier', birthDate: '2010-05-12', balance: 20.75, transactions: [] },
-  { firstName: 'Lena', lastName: 'Schmidt', birthDate: '2009-09-20', balance: 15.00, transactions: [] },
+  { firstName: 'Max', lastName: 'Mustermann', birthDate: '2011-01-01', balance: 10.50, transactions: [
+    { date: '2023-07-01 09:00', amount: 5.00 },
+    { date: '2023-07-02 09:00', amount: -1.50 },
+    // Weitere Transaktionen...
+  ] },
+  { firstName: 'Max', lastName: 'Meier', birthDate: '2010-05-12', balance: 20.75, transactions: [
+    { date: '2023-07-01 09:00', amount: 10.00 },
+    { date: '2023-07-02 09:00', amount: -1.00 },
+    // Weitere Transaktionen...
+  ] },
+  { firstName: 'Lena', lastName: 'Schmidt', birthDate: '2009-09-20', balance: 15.00, transactions: [
+    { date: '2023-07-01 09:00', amount: 7.00 },
+    { date: '2023-07-02 09:00', amount: -2.50 },
+    // Weitere Transaktionen...
+  ] },
+  { firstName: 'Tom', lastName: 'Muller', birthDate: '2009-04-25', balance: 12.00, transactions: [
+    { date: '2023-07-01 09:00', amount: 6.00 },
+    { date: '2023-07-02 09:00', amount: -2.00 },
+    // Weitere Transaktionen...
+  ] },
   // Weitere Kinder hinzufügen...
 ];
 
@@ -23,11 +40,15 @@ searchInput.addEventListener('input', () => {
   const results = database.filter(child => {
     const name = child.firstName.toLowerCase() + ' ' + child.lastName.toLowerCase();
     return name.includes(searchValue);
-  });
+  }).sort((a, b) => a.lastName.localeCompare(b.lastName));
   searchResults.innerHTML = '';
   results.forEach(child => {
     const div = document.createElement('div');
-    div.textContent = `${child.firstName} ${child.lastName}, geb. am ${child.birthDate}`;
+    const name = document.createElement('strong');
+    name.textContent = `${child.firstName} ${child.lastName}`;
+    const birthDate = document.createElement('span');
+    birthDate.textContent = child.birthDate;
+    div.append(name, birthDate);
     div.addEventListener('click', () => {
       selectedChild = child;
       childNameElement.textContent = `${child.firstName} ${child.lastName}`;
